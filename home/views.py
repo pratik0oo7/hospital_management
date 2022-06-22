37c1ade797ebe207a59a14f4fd87432b184d83d9
pratik0oo7 <pratikmendapra@gmail.com>
2022-06-22 09:50:08 +0530

    added ADMINCLICK, DOCTORCLICK, PATIENTCLICK VIEWS AND URLS

--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+#home pages views start here
+def home(request):
+    return HttpResponse("index page")
+
+
+def adminclick(request):
+    return HttpResponse("admin click")
+
+
+def doctorclick(request):
+    return HttpResponse("doctor click")
+
+
+def patientclick(request):
+    return HttpResponse("patient click")
+
+#home pages views end here
