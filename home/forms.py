47cb26916ba8880317d3929d6d04c553169dc98e
pratik0oo7 <pratikmendapra@gmail.com>
2022-06-27 12:40:46 +0530

    patientforms creation complate

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,17 @@
-from dataclasses import field
+from dataclasses import field, fields
 from django import forms
 from django.contrib.auth.models import User
 from . import models
+
+class patientuserform(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_nmae','last_name','username','password']
+        widgets={
+            'password':forms.PasswordInput()
+        }
+a
+class patientform(forms.ModelForm):
+    class Meta:
+        model=models.Patient
+        fields=['address','symptoms','profile_pic','mobile','status']
