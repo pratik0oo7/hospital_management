3b78e9e80129b732fcbefe51685987ad8f8c5910
pratik0oo7 <pratikmendapra@gmail.com>
2022-07-06 11:57:24 +0530

    added error resolved code

--- home/forms.py
+++ home/forms.py
@@ -3,6 +3,22 @@ from django.contrib.auth.models import User
 from . import models
 
 
+class loginForm(forms.Form):
+    username=forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control'
+            }
+        )
+    )
+    password=forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class':'form-control'
+            }
+        )
+    )
+
 class patientuserform(forms.ModelForm):
     class Meta:
         model = User
